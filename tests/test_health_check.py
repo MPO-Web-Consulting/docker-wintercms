e6542e399bfe7a3439cc280dbd46d624e30c510b
mik-p <mppritchard3@hotmail.com>
2024-01-27 17:00:31 +1100

    fix fpm tests health check command

--- tests/test_health_check.py
+++ tests/test_health_check.py
@@ -5,6 +5,10 @@ This file contains a test that checks the health of the WinterCMS container.
 import time
 import docker
 import pytest
+import logging
+
+# get the logger
+LOGGER = logging.getLogger(__name__)
 
 # Create a Docker client
 client = docker.from_env()
@@ -32,18 +36,28 @@ def test_container_health(image):
     :param image: The image of the container to test
     :return: None
     """
-    health_check_command = "curl --fail http://localhost/ || exit 1"
+    # if image CMD is apache then use curl to check health
+    # if image CMD is php-fpm then use php-fpm to check health
+    if client.images.pull(image).attrs['Config']['Cmd'] == ['apache2-foreground']:
+        health_check_command = "curl --fail http://localhost/ || exit 1"
+    elif client.images.pull(image).attrs['Config']['Cmd'] == ['php-fpm']:
+        health_check_command = "php-fpm -t || exit 1"
+    else:
+        pytest.fail("Image CMD not supported")
 
     # Run the container
-    container = client.containers.run(image, detach=True, healthcheck={
+    LOGGER.info("Running container %s", image)
+    container = client.containers.run(image, environment={"INIT_WINTER": "true"}, detach=True, healthcheck={
                                       "test": health_check_command, "interval": 30000000})
 
     try:
         # Wait for the container to become healthy
+        LOGGER.info("Checking container health")
         for _ in range(20):  # Timeout after 20 attempts
             if is_container_healthy(container):
+                LOGGER.info("Container is healthy")
                 break
-            time.sleep(1)
+            time.sleep(2)
         else:
             pytest.fail("Container did not become healthy")
 
